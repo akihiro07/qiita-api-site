e223f73186a60a84a861cea39c41c8c6263b21cb
Akihiro <marsstay0729@gmail.com>
2021-03-23 11:45:51 +0900

    Refactor: AButtonのテストコードをDRYに従ったコードに

--- test/button.spec.js
+++ test/button.spec.js
@@ -1,39 +1,26 @@
 import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import Component from '@/components/atoms/AButton.vue'
 
+const text = 'テキスト'
+const factory = (to) => {
+  return shallowMount(Component, {
+    propsData: {
+      text,
+      to,
+    },
+    stubs: { NuxtLink: RouterLinkStub },
+  })
+}
+
 describe('AButton.vue', () => {
-  // describe('textの型がstringか', () => {
-  //   test('result', () => {
-  //     const text = 12
-  //     const wrapper = shallowMount(Component, {
-  //       propsData: { text },
-  //     })
-  //     expect(wrapper.text()).toMatch(text)
-  //   })
-  // })
-
-  // test('propsのtoが空のときは<button>を表示', () => {
-
-  // })
-
   test('propsのtoが空ときは<button>が表示', () => {
-    const text = 'テキスト'
-    const to = ''
-    const wrapper = shallowMount(Component, {
-      propsData: { text, to },
-      stubs: { NuxtLink: RouterLinkStub },
-    })
+    const wrapper = factory('')
 
     expect(wrapper.find('button').exists()).toBe(true)
   })
 
   test('propsのtoが存在するときは<a>が表示', () => {
-    const text = 'テキスト'
-    const to = '/link/'
-    const wrapper = shallowMount(Component, {
-      propsData: { text, to },
-      stubs: { NuxtLink: RouterLinkStub },
-    })
+    const wrapper = factory('/link/')
 
     expect(wrapper.find('a').exists()).toBe(true)
   })
