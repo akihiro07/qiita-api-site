ca6edd1b6442774c0a07bd4ff0ad5d7b5ee366f5
Akihiro <marsstay0729@gmail.com>
2021-03-24 15:11:39 +0900

    Add: AInputのpropsが要素にバインドしているかテスト

--- test/input.spec.js
+++ test/input.spec.js
@@ -1,15 +1,44 @@
 import { shallowMount } from '@vue/test-utils'
 import Component from '@/components/atoms/AInput.vue'
 
+const text = '何かしら入力'
+const type = 'text'
+const placeholder = 'プレースホルダー'
+const factory = () => {
+  return shallowMount(Component, {
+    propsData: {
+      text,
+      type,
+      placeholder,
+    },
+  })
+}
+
 describe('AInput.vue', () => {
   test('inputイベントが発火したらemitが呼ばれる', () => {
-    const wrapper = shallowMount(Component)
-    const input = wrapper.find('input[type="text"]')
-    const text = '何かしら入力'
-    input.setValue(text)
+    const wrapper = factory()
+    const inputElement = wrapper.find('input')
+
+    inputElement.setValue(text)
     // ↑エイリアス
-    input.element.value = text
-    input.trigger('input')
+    // inputElement.element.value = text
+    // inputElement.trigger('input')
     expect(wrapper.emitted()['update:text'][0][0]).toBe(text)
+  })
+
+  test('propsのtypeを<input>にbind', () => {
+    const wrapper = factory()
+    const propsType = wrapper.props().type
+    const inputElement = wrapper.find('input')
+
+    expect(inputElement.attributes().type).toBe(propsType)
+  })
+
+  test('propsのplaceholderを<input>にbind', () => {
+    const wrapper = factory()
+    const propsPlaceholder = wrapper.props().placeholder
+    const inputElement = wrapper.find('input')
+
+    expect(inputElement.attributes().placeholder).toBe(propsPlaceholder)
   })
 })
