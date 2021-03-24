60dee06d2a1fe7406d1760bff7e4393f8b191bb3
Akihiro <marsstay0729@gmail.com>
2021-03-24 09:05:57 +0900

    Tweak: tagコンポーネントのstyleを微調整

--- components/molecules/MTagInput.vue
+++ components/molecules/MTagInput.vue
@@ -36,7 +36,11 @@ export default defineComponent({
     // objectのkeyを`name`->`text`に変換(packageの使用上)
     const formatQiitaTags = computed(() => {
       const filterData = props.qiitaTags.map((item) => {
-        return { text: item.name }
+        return {
+          text: item.name,
+          style:
+            'color: #56c1da; background-color: transparent; border: 1px solid #52bd95',
+        }
       })
       return filterData
     })
